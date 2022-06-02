f3b3995d8de322c8f3dde5a262f278c103c5e068
voringfossen <71556984+voringfossen@users.noreply.github.com>
2022-06-02 20:00:30 +0200

    Update settings.py

--- saleor/settings.py
+++ saleor/settings.py
@@ -52,7 +52,7 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1"
+_DEFAULT_CLIENT_HOSTS = "saldjango.herokuapp.com"
 
 ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
 if not ALLOWED_CLIENT_HOSTS:
